58ba1bbedc2c80f11ec9cc25368a064cf84703a0
Dave Mandelin <dmandelin@gmail.com>
2025-03-02 06:51:54 -0800

    Fix population pyramid display bugs.

--- src/components/PopulationPyramid.svelte
+++ src/components/PopulationPyramid.svelte
@@ -28,8 +28,8 @@
   <div class="pyramid">
     {#each clan.slices.toReversed() as slice, index}
       <div class="slice">
-        <div class="barc"><div class="bar" style="margin-left: auto; width: {slice[0]*20}%; background-color: #eaa;">{slice[0]}</div></div>
-        <div class="barc"><div class="bar" style="width: {slice[1]*20}%; background-color: #aae;">{slice[1]}</div></div>
+        <div class="barc"><div class="bar" style="text-align: right; margin-left: auto; width: {slice[0]*15}%; background-color: #eaa;">{slice[0]}</div></div>
+        <div class="barc"><div class="bar" style="width: {slice[1]*15}%; background-color: #aae;">{slice[1]}</div></div>
       </div>
     {/each}
   </div>
