28b47ff34f059da249e3129d6cd16fc6999efa48
Dave Mandelin <dmandelin@gmail.com>
2025-03-06 10:40:06 -0800

    Rearrange layout.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -58,6 +58,7 @@
     .topPanel {
         display: flex;
         gap: 2em;
+        margin-bottom: 1em;
     }
 
     .topPanel h3 {
@@ -94,58 +95,60 @@
 
 <div class="mapRow">
     <Map />
-    <div class="topPanel">
-        <div>
-            <h3>{data.year} - {data.totalPopulation} people</h3>
-            <button onclick={click}>Advance</button>
-        </div>
-        <div>
-            <h3 style="text-align: center">{data.festivalMessage}</h3>
-            <img src="/festival.webp" alt="Festival" height="120" width="200"/>
+    <div>
+        <div class="topPanel">
+            <div>
+                <h3 style="text-align: center">{data.festivalMessage}</h3>
+                <img src="/festival.webp" alt="Festival" height="120" width="200"/>
+            </div>
+            <div>
+                <h3>{data.year} - {data.totalPopulation} people</h3>
+                <button onclick={click}>Advance</button>
+            </div>
         </div>
         <h4>{data.message}</h4>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Clan</th>
+                <th class="ra">Size</th>
+                <th class="ra">Skill</th>
+                <th class="ra">Know</th>
+                <th class="ra">Qual</th>
+                <th class="ra">Int</th>
+                <th class="ra">FesB</th>
+                <th class="ra">Fes</th>
+                <th class="ra">EQual</th>
+                <th class="ra">Pres</th>
+                <th class="ra">Hap</th>
+                <th class="ra">EBR</th>
+                <th class="ra">EDR</th>
+                <th class="ra">EPC</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each data.clans as clan}
+                <tr>
+                    <td style:color={clan.color}>{clan.name}</td>
+                    <td class="ra">{clan.size}</td>
+                    <td class="ra">{clan.skill}</td>
+                    <td class="ra">{clan.knowledge}</td>
+                    <td class="ra">{clan.quality}</td>
+                    <td class="ra">{clan.interactionModifier}</td>
+                    <td class="ra">{clan.festivalBehavior.name}</td>
+                    <td class="ra">{clan.festivalModifier}</td>
+                    <td class="ra">{clan.effectiveQuality}</td>
+                    <td class="ra">{clan.prestige}</td>
+                    <td class="ra">{clan.happiness}</td>
+                    <td class="ra">{clan.expectedPopulationChange[0]}</td>
+                    <td class="ra">{clan.expectedPopulationChange[1]}</td>
+                    <td class="ra">{clan.expectedPopulationChange[2]}</td>
+                </tr>
+            {/each}
+    </table>
     </div>
 </div>
-
-<table>
-    <thead>
-        <tr>
-            <th>Clan</th>
-            <th class="ra">Size</th>
-            <th class="ra">Skill</th>
-            <th class="ra">Know</th>
-            <th class="ra">Qual</th>
-            <th class="ra">Int</th>
-            <th class="ra">FesB</th>
-            <th class="ra">Fes</th>
-            <th class="ra">EQual</th>
-            <th class="ra">Pres</th>
-            <th class="ra">Hap</th>
-            <th class="ra">EBR</th>
-            <th class="ra">EDR</th>
-            <th class="ra">EPC</th>
-        </tr>
-    </thead>
-    <tbody>
-        {#each data.clans as clan}
-            <tr>
-                <td style:color={clan.color}>{clan.name}</td>
-                <td class="ra">{clan.size}</td>
-                <td class="ra">{clan.skill}</td>
-                <td class="ra">{clan.knowledge}</td>
-                <td class="ra">{clan.quality}</td>
-                <td class="ra">{clan.interactionModifier}</td>
-                <td class="ra">{clan.festivalBehavior.name}</td>
-                <td class="ra">{clan.festivalModifier}</td>
-                <td class="ra">{clan.effectiveQuality}</td>
-                <td class="ra">{clan.prestige}</td>
-                <td class="ra">{clan.happiness}</td>
-                <td class="ra">{clan.expectedPopulationChange[0]}</td>
-                <td class="ra">{clan.expectedPopulationChange[1]}</td>
-                <td class="ra">{clan.expectedPopulationChange[2]}</td>
-            </tr>
-        {/each}
-</table>
 
 <div class="pyramids">
     {#each data.clans as clan}
