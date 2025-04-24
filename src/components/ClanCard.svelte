d4cffd39c79c29e41dfcd69ce59725600b9b32cc
Dave Mandelin <dmandelin@gmail.com>
2025-04-24 15:39:21 -0700

    Simple clan cards.

---
+++ src/components/ClanCard.svelte
@@ -0,0 +1,30 @@
+<script>
+    let { clan } = $props();
+</script>
+
+<style>
+    #top {
+        background-color: #f3edd8;
+        border: 1px solid #62531d;
+        border-radius: 5px;
+        padding: 0.25rem;
+    }
+</style>
+
+<div id="top">
+    <table>
+        <tbody>
+            <tr>
+                <td colspan="2" style:color={clan.color} style:font-weight="bold" style:text-align="center">{clan.name}</td>
+            </tr>
+            <tr>
+                <td>People</td>
+                <td>{clan.size}</td>
+            </tr>
+            <tr>
+                <td>QoL</td>
+                <td>{clan.qol.toFixed()}</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
--- src/components/Settlement.svelte
+++ src/components/Settlement.svelte
@@ -1,4 +1,6 @@
 <script>
+    import ClanCard from "./ClanCard.svelte";
+
     let { settlement } = $props();
 </script>
 
@@ -10,8 +12,20 @@
     h1 {
         margin: 0 0 1rem 0;
     }
+
+    h3, h4 {
+        margin: 0.5rem 0;
+    }
 </style>
 
 <div id="top">
 <h1>{settlement.name}</h1>
+<h3>{settlement.size} people</h3>
+
+<h4 style="text-align: center; border-bottom: 1px solid grey">The {settlement.clans.length} clans</h4>
+<div style="display: flex; gap: 0.25rem;">
+    {#each settlement.clans as clan}
+    <ClanCard clan={clan} />
+    {/each}
+</div>
 </div>
