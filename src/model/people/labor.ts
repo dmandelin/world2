7797c621ea053a1f41f5dad0c012903cc9df964c
Dave Mandelin <dmandelin@gmail.com>
2025-07-29 19:56:13 -0700

    Add a planned alllocation concept so we can create mandatory allocations separately.

--- src/model/people/labor.ts
+++ src/model/people/labor.ts
@@ -5,14 +5,21 @@ import { Clan } from './people';
 import { SkillDefs, type SkillDef } from './skills';
 
 export class LaborAllocation {
-    // value is fraction and values must sum to 1
-    readonly allocs: Map<SkillDef, number> = new Map<SkillDef, number>();
+    // Planned allocations for labor left over after maintenance and other 
+    // required allocations. Value is fraction and values must sum to 1.
+    readonly planned_: Map<SkillDef, number> = new Map<SkillDef, number>();
 
     constructor(readonly clan: Clan) {
         const r = clamp(normal(0.2, 0.1), 0, 1);
-        this.allocs.set(SkillDefs.Agriculture, r);
-        this.allocs.set(SkillDefs.Fishing, 1 - r);
+        this.planned_.set(SkillDefs.Agriculture, r);
+        this.planned_.set(SkillDefs.Fishing, 1 - r);
     }
 
-    get workers(): number { return this.clan.population / 3; }
+    get allocs(): Map<SkillDef, number> {
+        return this.planned_;
+    }
+
+    get workers(): number { 
+        return this.clan.population / 3;
+    }
 }
