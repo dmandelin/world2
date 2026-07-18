b3edf7cfb8063c37360058b3bb12a888245d1513
Dave Mandelin <dmandelin@gmail.com>
2026-07-18 07:15:11 -0700

    Notes on next updates to skill changes.

--- src/model/people/skillchange.ts
+++ src/model/people/skillchange.ts
@@ -25,20 +25,71 @@ import { getRespect } from '../relations/respect';
 //       In some cases, clans may mistakenly learn something
 //       that makes them worse.
 //
-// Loss factors are generally scaled by initial skill value, but
-// gain factors are unscaled. At least one should be scaled so
-// that skill moves toward an equilibrium value rather than increasing
-// without limit. Tuning is easier when we scale only one. It also
-// seems to be theoretically justified, as some sources disagree
-// whether learning and innovation move toward as asymptote, or if
-// the more skilled advance as fast or faster. Conversely, the more
-// skill you have, the more resources on some level it must take to
-// maintain.
+// The skill rating can be thought of in three different ways:
+// - log(productivity from skill)
+// - Elo rating for contests
+// - The number of "tricks" or "lessons" learned
 //
-// People who are working together tend to learn and notice the
-// same kinds of things, so N times as many people on task doesn't
-// mean N times the skill learning. However, there is some increase
-// for having more.
+// The latter view suggests that loss from error is proportional
+// to skill, while gains are fixed. That means the skill update
+// function looks something like
+//
+//   S1 = r * S0 + g
+//
+// where r is the retention factor (r = 1 - e, the error factor).
+// At equilibrium (good to know for tuning), S0 and S1 are equal:
+//
+//   S = r * S + g
+//   (1 - r) * S = g
+//   S = g / (1 - r) = g / e
+//
+// Factors affecting skill change rates in the current model:
+// - Clan size
+//   - This is a very complicated consideration, and we don't
+//     try to understand in detail, but here are a few points
+//   - More people means more total error (variance) as they learn
+//     separately, but they can also "error-correct" each other,
+//     so they could have less change, but beyond some point,
+//     they won't completely sync. We can assume they roughly
+//     balance and ignore this effect, but later, if we want
+//     more size tradeoffs, a slight reduction in both loss due
+//     to error and learning seems warranted.
+//   - More people means more chance to run into new situations
+//     to learn from, and more people to try to solve new problems,
+//     but also more coordination problems with that, and since
+//     they do a lot of work together, they'll somewhat tend to
+//     encounter the same problems.
+//   - We could go a lot of ways with this, but my intuition is
+//     that large clans may have more resources to innovate with,
+//     but small clans will find it easier to change. Exactly
+//     how that shakes out depends on internal group structure
+//     (e.g., tradition-based clan vs science lab). In our
+//     setting, it makes sense to give a larger clan less change
+//     in both directions, but we could ignore this.
+// - Effort allocation
+//   - Also complicated but some points.
+//   - If a clan is 70% farming and 30% fishing, we assume most
+//     families in the clan are doing about that same split: we
+//     assume they have a common way of life together, but with
+//     some scope for mild specialization.
+//   - Clans contain different age groups, which will be at
+//     different skill levels, making things very complicated.
+//     But I did an analysis of equilibrium clan productivity,
+//     and found that it depends on skill ramp-up time: if it's
+//     very fast, clans doing it only a little bit can be fully
+//     productive. Conversely, if it takes a lifetime to learn,
+//     skill, and thus productivity, will always be low if doing
+//     the activity 5% of the time.
+//     - We'll assume our initial farming and fishing processes are
+//       not too hard, and people can become productive relatively
+//       quickly, but with some meaningful time taken.
+//       - Really, we could assume our farmers' skills are initially
+//         very basic, but with low capital costs (alluvium) and
+//         high returns (farming the richest soils). That suggests
+//         we let skills be kind of low but boost farm productivity
+//         overall (until we get differential land quality)
+//     - Skill requirements should probably go up as technology
+//       goes up (depending on the technology)
 //
 // For now, clans with higher intelligence *don't* learn faster,
 // because there is no tradeoff. It doesn't make sense to have
@@ -122,6 +173,8 @@ export class ClanSkillChange {
         }
 
         // Learn by observation. This should roughly balance error at skill 50.
+        // TODO - might want less observation learning if they're imitating,
+        //        but still allow some
         const clanSkillFactor = skillDef.clanSkill ? 1.5 : 1;
         const deltaFromObservation = clanSkillFactor * this.effortFactor *
             (5 + 5 * (Math.random() - Math.random()));
